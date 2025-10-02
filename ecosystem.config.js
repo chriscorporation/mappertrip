e91932571265e3197991219e5e02379ad138316f
root <root@meet.vacantesdigitales.com>
2025-10-02 08:44:25 +0000

    from server

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,6 +2,8 @@ module.exports = {
   apps: [{
     name: 'mappertrip.com',
     script: 'npm',
+    args: 'start',
+    cwd: '/home/mappertrip.com',
     env: {
       NODE_ENV: 'production',
       PORT: 3025
